174706e2c35db77348cf2e9700cde178ee860b37
Rohan Verma <vermarohan1022@gmail.com>
2018-10-12 11:47:18 +0530

    ass

---
+++ interface.java
@@ -0,0 +1,63 @@
+interface Animals {
+
+void speak();
+
+void eat();	
+
+}
+
+class Dog implements Animals{
+
+public void speak() {
+
+System.out.println("Dog bark");
+
+}
+
+public void eat() {
+
+System.out.println("Dog eat bones");
+
+}
+
+}
+
+class cat implements Animals{
+
+public void speak() {
+
+System.out.println("Cat meow");
+
+}
+
+public void eat() 
+
+{
+
+System.out.println("Cat veggies");
+
+}
+
+}
+
+class interface{
+
+public static void main(String[] args) {
+
+Animals d=new Dog();
+
+d.speak();
+
+d.eat();
+
+d=new cat();
+
+System.out.println("\n---------------------\n");
+
+      d.speak();
+
+      d.eat();
+
+}
+
+}
